e359327601c8e477446875e98e97cbb9ee73bb1e
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-02 20:32:11 +0800

    update GetAuthorizeURL

--- component/get_authorize_url.go
+++ component/get_authorize_url.go
@@ -8,5 +8,5 @@ import (
 // GetAuthorizeURL 获取授权页面的URL地址
 func GetAuthorizeURL(authorize *model.Authorize) string {
 	v, _ := query.Values(authorize)
-	return v.Encode()
+	return "https://open.weixin.qq.com/connect/oauth2/authorize?" + v.Encode() + "&response_type=code#wechat_redirect"
 }
