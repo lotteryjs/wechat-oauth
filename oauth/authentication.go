2b3282fc59b222a4048a40e11822d2aa56125840
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-03 20:26:07 +0800

    add oauth

---
+++ oauth/authentication.go
@@ -0,0 +1,30 @@
+package oauth
+
+import (
+	"github.com/google/go-querystring/query"
+	"github.com/lotteryjs/wechat-oauth/model"
+)
+
+// Auth is the provider
+type Auth struct {
+	// 公众号的唯一标识
+	appid string
+	// 公众号的appsecret
+	appsecret string
+}
+
+// New 构造 OAuth 方法
+func New(appid string, appsecret string) *Auth {
+	return &Auth{appid, appsecret}
+}
+
+// GetAuthorizeURL 获取授权页面的URL地址
+func (a *Auth) GetAuthorizeURL(redirect string, state string, scope string) string {
+	model := &model.Authorize{
+		Appid:       a.appid,
+		RedirectURI: redirect,
+		Scope:       scope,
+		State:       state}
+	v, _ := query.Values(model)
+	return "https://open.weixin.qq.com/connect/oauth2/authorize?" + v.Encode() + "&response_type=code#wechat_redirect"
+}
